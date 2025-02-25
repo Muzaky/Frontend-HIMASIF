ccfb4c569b7433369ec181f351013a2fcbdfb7b7
Muzaky <afif050503@gmail.com>
2025-02-25 13:53:56 +0700

    ppp

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
       {
-        name: 'vue-app',
-        script: './node_modules/@vue/cli-service/bin/vue-cli-service.js',
-        args: 'serve',
+        name: 'nuxt-app', // Application name
+        script: '.output/server/index.mjs', // Entry point of your Nuxt app
+        exec_mode: 'cluster', // Optional: Enables clustering for better performance
+        instances: 'max', // Optional: Scales based on available CPU cores
         env: {
-          NODE_ENV: 'production',
-          PORT: 80, // Set port 80
-          HOST: '0.0.0.0', // Allow external access
+          NODE_ENV: 'production', // Set environment to production
+          NITRO_PORT: 80, // Serve on port 80
         },
       },
     ],
